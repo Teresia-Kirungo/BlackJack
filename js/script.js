0c5cd043eda314c980cbb2098da4c1b1222337c3
Terri-kirungo <terrykirungo@gmail.com>
2020-02-10 19:27:24 -0800

    Added index.html, css and js directory

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//Starting our blackjack app
+//  by Teresia Kirung'o
+
+//variables
+let card1 = "Ace of Spades",
+    card2 = "Ten of Hearts";
+
+//what we log out at the console    
+console.log("Welcome to Blackjack!");
+
+console.log("You are dealt: ");
+console.log("   " + card1);
+console.log("   " + card2);
+
